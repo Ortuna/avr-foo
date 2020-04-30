0411d34d352b435d22924bbfad7b5763d0c7a429
Sumeet Singh <ortuna@gmail.com>
2020-04-29 22:34:39 -0700

    Using interrupts

--- main.c
+++ main.c
@@ -1,31 +1,30 @@
 #define F_CPU 16000000
 #include <avr/io.h>
+#include <avr/interrupt.h>
  
 uint8_t timer_overflow = 0;
+
+ISR(TIMER0_OVF_vect) {
+  timer_overflow++;
+  if(timer_overflow == 6) {
+    timer_overflow = 0;
+
+    PORTB ^= (0x01 << PB5);
+  }
+}
 
 int main()
 {
   DDRB |= (1 << PB5);    // Make pin 13 be an output.  
 
-  //TIMSK0 = (0x01 << TOIE0);
+  TIMSK0 = (0x01 << TOIE0);
   TCNT0  = 0x00;
   OCR0A  = 0xFF;
 
-  TCCR0A = 0x00;
+  TCCR0A |= (0x01 << WGM00) | (0x01 << WGM01);
   TCCR0B |= (0x01 << CS02) | (0x01 << CS00);
 
-  while(1)
-  {
-    if((TIFR0 & 0x01) == 1) {
-      TIFR0 |= (0x1 << TOV0);
+  sei();
 
-      timer_overflow++;
-      if(timer_overflow == 6) {
-        timer_overflow = 0;
-
-        PORTB ^= (0x01 << PB5);
-      }
-
-    }
-  }
+  while(1) { }
 }
