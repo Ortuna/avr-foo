72eccc5497086dc4ceca39dcf58264712ae2f3f6
Sumeet Singh <ortuna@gmail.com>
2020-05-01 23:54:25 -0700

    Saving Spot

--- main.c
+++ main.c
@@ -1,30 +1,30 @@
 #define F_CPU 16000000
 #include <avr/io.h>
 #include <avr/interrupt.h>
- 
+
 uint8_t timer_overflow = 0;
 
 ISR(TIMER0_OVF_vect) {
-  timer_overflow++;
-  if(timer_overflow == 6) {
+  if(timer_overflow > 6) {
     timer_overflow = 0;
 
     PORTB ^= (0x01 << PB5);
+    PORTD ^= (0x01 << PD2);
   }
+
+  timer_overflow++;
 }
 
 int main()
 {
-  DDRB |= (1 << PB5);    // Make pin 13 be an output.  
+  DDRB |= (0x01 << PB5);
+  DDRD |= (0x01 << PD2);
+
+  TCCR0A = 0x00;
+  TCCR0B = (0x01 << CS01) | (0x01 << CS00);
 
   TIMSK0 = (0x01 << TOIE0);
-  TCNT0  = 0x00;
-  OCR0A  = 0xFF;
-
-  TCCR0A |= (0x01 << WGM00) | (0x01 << WGM01);
-  TCCR0B |= (0x01 << CS02) | (0x01 << CS00);
 
   sei();
-
   while(1) { }
 }
